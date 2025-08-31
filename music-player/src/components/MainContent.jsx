5ccbfc0e587126619bce4aa62b1ba50364a1e1a6
dwin504 <dwintez254@gmail.com>
2025-08-31 10:58:57 +0300

    added a line to export Header component

--- music-player/src/components/MainContent.jsx
+++ music-player/src/components/MainContent.jsx
@@ -2,3 +2,15 @@ import React, { useState } from 'react';
 import { useSearch } from '../context/SearchContext.jsx';
 import TrackList from './TrackList.jsx';
 import './MainContent.css';
+const MainContent = ({ onTrackSelect }) => {
+  const { searchResults, popularTracks, isLoading, error, performSearch } = useSearch();
+  const [localQuery, setLocalQuery] = useState('');
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    performSearch(localQuery);
+  };
+
+  const handleInputChange = (e) => {
+    setLocalQuery(e.target.value);
+  };
