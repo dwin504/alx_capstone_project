6622193e3ef05901018043769b1bbc9d366c9863
dwin504 <dwintez254@gmail.com>
2025-08-31 15:34:31 +0300

    upaddated the app.jsx file

--- music-player/src/App.jsx
+++ music-player/src/App.jsx
@@ -7,3 +7,38 @@ import PlayerControls from './components/PlayerControls.jsx';
 import Header from './components/Header.jsx';
 import './App.css';
 
+function App() {
+  const [currentTrack, setCurrentTrack] = React.useState(null);
+  const [isPlaying, setIsPlaying] = React.useState(false);
+
+  const handleSearch = (query) => {
+    console.log('Searching for:', query);
+  };
+
+  const handleTrackSelect = (track) => {
+    setCurrentTrack(track);
+    setIsPlaying(true);
+  };
+
+  return (
+    <AppProvider>
+      <SearchProvider>
+        <div className="app">
+          <Sidebar />
+          <div className="main-content-wrapper">
+            <Header onSearch={handleSearch} />
+            <MainContent onTrackSelect={handleTrackSelect} />
+          </div>
+          <PlayerControls 
+            currentTrack={currentTrack} 
+            isPlaying={isPlaying}
+            onPlay={() => setIsPlaying(true)}
+            onPause={() => setIsPlaying(false)}
+          />
+        </div>
+      </SearchProvider>
+    </AppProvider>
+  );
+}
+
+export default App;
