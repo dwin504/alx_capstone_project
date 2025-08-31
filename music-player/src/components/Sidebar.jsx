947b8b08a0ce557ba1ab5c8f36690e41b3cded51
dwin504 <dwintez254@gmail.com>
2025-08-31 13:50:03 +0300

    added popular section to sidebar also create a playlist section

--- music-player/src/components/Sidebar.jsx
+++ music-player/src/components/Sidebar.jsx
@@ -33,3 +33,71 @@ const Sidebar = () => {
           </div>
         </div>
       </div>
+       <div className="sidebar-section">
+        <h2 className="section-title">Popular</h2>
+        <div className="popular-list">
+          <div className="popular-item">
+            <span className="track-number">1</span>
+            <div className="track-info">
+              <span className="track-name">Love Song</span>
+              <span className="track-duration">3:23</span>
+            </div>
+            <span className="track-more">---</span>
+          </div>
+          <div className="popular-item">
+            <span className="track-number">2</span>
+            <div className="track-info">
+              <span className="track-name">Kombucha Burps</span>
+              <span className="track-duration">3:36</span>
+            </div>
+            <span className="track-more">---</span>
+          </div>
+          <div className="popular-item">
+            <span className="track-number">3</span>
+            <div className="track-info">
+              <span className="track-name">On The Regular</span>
+              <span className="track-duration">2:39</span>
+            </div>
+            <span className="track-more">---</span>
+          </div>
+          <div className="popular-item">
+            <span className="track-number">4</span>
+            <div className="track-info">
+              <span className="track-name">El Sambroso</span>
+              <span className="track-duration">3:26</span>
+            </div>
+            <span className="track-more">---</span>
+          </div>
+          <div className="popular-item">
+            <span className="track-number">5</span>
+            <div className="track-info">
+              <span className="track-name">Mask Off Soul Flip</span>
+              <span className="track-duration">6:29</span>
+            </div>
+            <span className="track-more">---</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="sidebar-section">
+        <h2 className="section-title">Mood</h2>
+        <div className="mood-list">
+          <div className="mood-item">
+            <span className="mood-genre">Ghetto Rap</span>
+            <span className="mood-track">Twerk Baby</span>
+          </div>
+          <div className="mood-item">
+            <span className="mood-genre">Party Breaker</span>
+            <span className="mood-track">Valentine Vibe</span>
+          </div>
+        </div>
+      </div>
+
+      <button className="create-playlist-btn">
+        Create playlist
+      </button>
+    </aside>
+  );
+};
+
+export default Sidebar;
